456c0ff06decbf9d95b7515c423621b95f7b9ecb
Kamil Sadigov <101421625+Sadigov26@users.noreply.github.com>
2024-06-11 21:52:14 +0400

    Add files via upload

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../../components/Header/Header'
+import Hero from '../../components/Hero/Hero'
+import Looking from '../../components/Looking/Looking'
+
+const Home = () => {
+    return (
+        <div>
+            <Header />
+            <Hero />
+            <Looking />
+        </div>
+    )
+}
+
+export default Home
