ecd26cf40af746d5d61da51f0a2eaeed14979fd2
Kamil Sadigov <101421625+Sadigov26@users.noreply.github.com>
2024-06-14 03:56:44 +0400

    Add files via upload

---
+++ components/Banner/Banner.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import styles from './Banner.module.scss'
+import img1 from '../../about/editors1@3x.e0b9b07f.png'
+import img2 from '../../about/editors2@3x.fc1a8fcc.png'
+import img3 from '../../about/editors3@3x.0e716ea7.png'
+import img4 from '../../about/editors4@3x.0a146aef.png'
+import img5 from '../../about/editors5@3x.55520542.png'
+import img6 from '../../about/editors6@3x.152133df.png'
+import img7 from '../../about/editors7@3x.7a76c89b.png'
+import img8 from '../../about/editors8@3x.1d22d3c1.png'
+
+const Banner = () => {
+    return (
+        <div>
+            <div className={styles.Banner}>
+                <div className={styles.BannerContainer}>
+                    <div className={styles.BannerContainerTop}>
+                        <h1>The best of the best.</h1>
+                        <p>Our editors are always on the lookout for jaw dropping content for you to discover and stay inspired. Check back weekly to see what’s new.</p>
+                        <button>View Editors' Choice</button>
+                    </div>
+                    <div className={styles.BannerContainerBottom}>
+                        <div className={styles.BannerContainerBottomImg}>
+                            <div className={styles.imageBanner}>
+                                <img src={img1} alt="" />
+                                <p className={styles.BannerImageDrop}>Photo by Hayden Scott</p>
+                            </div>
+                            <div className={styles.imageBanner}>
+                                <img src={img2} alt="" />
+                                <p className={styles.BannerImageDrop}>Photo by Hayden Scott</p>
+                            </div>
+                            <div className={styles.imageBanner}>
+                                <img src={img3} alt="" />
+                                <p className={styles.BannerImageDrop}>Photo by Hayden Scott</p>
+                            </div>
+                            <div className={styles.imageBanner}>
+                                <img src={img4} alt="" />
+                                <p className={styles.BannerImageDrop}>Photo by Hayden Scott</p>
+                            </div>
+                            <div className={styles.imageBanner}>
+                                <img src={img5} alt="" />
+                                <p className={styles.BannerImageDrop}>Photo by Hayden Scott</p>
+                            </div>
+                            <div className={styles.imageBanner}>
+                                <img src={img6} alt="" />
+                                <p className={styles.BannerImageDrop}>Photo by Hayden Scott</p>
+                            </div>
+                            <div className={styles.imageBanner}>
+                                <img src={img7} alt="" />
+                                <p className={styles.BannerImageDrop}>Photo by Hayden Scott</p>
+                            </div>
+                            <div className={styles.imageBanner}>
+                                <img src={img8} alt="" />
+                                <p className={styles.BannerImageDrop}>Photo by Hayden Scott</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Banner
---
+++ components/Footer/Footer.jsx
@@ -0,0 +1,82 @@
+import React from 'react'
+import styles from './Footer.module.scss'
+import playmarket from '../../about/image.png'
+import appstore from '../../about/image copy.png'
+import { FaFacebookF } from "react-icons/fa6";
+import { FaTwitter } from "react-icons/fa";
+import { IoLogoInstagram } from "react-icons/io5";
+import { FaPinterestP } from "react-icons/fa";
+
+const Footer = () => {
+    return (
+        <div>
+            <div className={styles.Footer}>
+                <div className={styles.FooterContainerLarge}>
+                    <div className={styles.FooterContainer}>
+                        <div className={styles.FooterContainerTop}>
+                            <div style={{ display: "flex" }}>
+                                <div className={styles.FooterNavbar}>
+                                    <h3>Company</h3>
+                                    <ul>
+                                        <li><a href="#">Newsroom</a></li>
+                                        <li><a href="#">About us</a></li>
+                                        <li><a href="#">Careers</a></li>
+                                    </ul>
+                                </div>
+                                <div className={styles.FooterNavbar}>
+                                    <h3>By community</h3>
+                                    <ul>
+                                        <li><a href="#">Popular photos</a></li>
+                                        <li><a href="#">Editors' Choice</a></li>
+                                        <li><a href="#">Quests</a></li>
+                                        <li><a href="#">Portfolio</a></li>
+                                    </ul>
+                                </div>
+                                <div className={styles.FooterNavbar}>
+                                    <h3>Licensing</h3>
+                                    <ul>
+                                        <li><a href="#">About Licensing</a></li>
+                                        <li><a href="#">Become a Contributor</a></li>
+                                        <li><a href="#">Submission requirements</a></li>
+                                        <li><a href="#">Content types</a></li>
+                                        <li><a href="#">Distribution</a></li>
+                                    </ul>
+                                </div>
+                                <div className={styles.FooterNavbar}>
+                                    <h3>Social</h3>
+                                    <ul>
+                                        <li><a style={{ display: "flex", alignItems: "center", gap: "15px" }} href="https://www.facebook.com/500px" target="_blank"><FaFacebookF /> Facebook</a></li>
+                                        <li><a style={{ display: "flex", alignItems: "center", gap: "15px" }} href="https://x.com/500px?mx=2" target="_blank"><FaTwitter /> Twitter</a></li>
+                                        <li><a style={{ display: "flex", alignItems: "center", gap: "15px" }} href="https://www.instagram.com/500px/" target="_blank"><IoLogoInstagram /> Instagram</a></li>
+                                        <li><a style={{ display: "flex", alignItems: "center", gap: "15px" }} href="https://www.pinterest.ca/500px/" target="_blank"><FaPinterestP /> Pinterest</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className={styles.FooterApp}>
+                                <h3>Download the app</h3>
+                                <div className={styles.getTheAppStore}>
+                                    <a href="https://apps.apple.com/us/app/500px-photo-sharing-community/id471965292"><img src={appstore} alt="" /></a>
+                                    <a href="https://play.google.com/store/apps/details?id=com.fivehundredpx.viewer"><img src={playmarket} alt="" /></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={styles.FooterContainerBottom}>
+                            <div className={styles.FooterContainerLeft}>
+                                <h1>© 500px</h1>
+                            </div>
+                            <div className={styles.FooterContainerRight}>
+                                <ul>
+                                    <li><a href="#">Terms</a></li>
+                                    <li><a href="#">Privacy</a></li>
+                                    <li><a href="#">Support</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
