51dd7023cf44df36e8bf584ee6030434e58d1691
Kamil Sadigov <101421625+Sadigov26@users.noreply.github.com>
2024-06-14 17:32:17 +0400

    ADD PAGE {grants}
    /grants

--- src/pages/CommercialGrants/CommercialGrants.jsx
+++ src/pages/CommercialGrants/CommercialGrants.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
+import Grants from '../../components/Grants/Grants'
 
 const CommercialGrants = () => {
     return (
-        <div style={{ width: "100%", gap: "100px", minHeight: "100vh", display: "flex", flexDirection: "column" }}>
+        <div style={{ width: "100%", gap: "80px", minHeight: "100vh", display: "flex", flexDirection: "column" }}>
             <Header />
-            
+            <Grants/>
             <Footer />
         </div>
     )
