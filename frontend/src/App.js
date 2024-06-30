ab7128b2b971ca8fcdf5f553614cfa0dc219954d
Kamil Sadigov <101421625+Sadigov26@users.noreply.github.com>
2024-07-01 01:38:40 +0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,16 +3,13 @@ import Router from './Router/Router';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import { ToastContainer } from 'react-toastify';
-import { SpeedInsights } from '@vercel/speed-insights/react'; // Import SpeedInsights component
 
 const App = () => {
   return (
     <div>
       <Provider store={store}>
-        <SpeedInsights> {/* Include SpeedInsights component */}
           <Router />
           <ToastContainer />
-        </SpeedInsights>
       </Provider>
     </div>
   );
