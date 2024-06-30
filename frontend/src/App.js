c14bb335f6e6477ea1fe89c52381dce52734618b
Kamil Sadigov <101421625+Sadigov26@users.noreply.github.com>
2024-07-01 01:09:15 +0400

    Update vvercel

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,21 @@
-import React from 'react'
-import Router from './Router/Router'
-import { Provider } from 'react-redux'
-import store from './redux/store'
-import { ToastContainer } from 'react-toastify'
+import React from 'react';
+import Router from './Router/Router';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import { ToastContainer } from 'react-toastify';
+import { SpeedInsights } from '@vercel/speed-insights/react'; // Import SpeedInsights component
 
 const App = () => {
   return (
     <div>
       <Provider store={store}>
-
-      <Router/>
-      <ToastContainer/>
+        <SpeedInsights> {/* Include SpeedInsights component */}
+          <Router />
+          <ToastContainer />
+        </SpeedInsights>
       </Provider>
-      
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
