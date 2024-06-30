2c57974a64874f748bc915edc98e7f821733b61c
Kamil Sadigov <101421625+Sadigov26@users.noreply.github.com>
2024-07-01 00:18:31 +0400

    deploy vercel app

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:3000', 
+    origin: 'https://500px-eight.vercel.app/', 
     credentials: true,
 }));
 app.use(express.urlencoded({ extended: true }));
